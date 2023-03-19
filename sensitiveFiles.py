4c882c9658e07966d0ff3b908f1fed168c786753
Karma Nakba <nerdynerd0905@gmail.com>
2023-03-20 00:50:36 +0530

    sensitive urls code

---
+++ sensitiveFiles.py
@@ -0,0 +1,11 @@
+import subprocess,re
+
+def hiddenFiles(targetName):
+    result = subprocess.check_output(['waybackurls',targetName,'|','uniq'])
+    hidFilesList = []
+    for i in (re.findall(r'(.*\.js|.*\.php|.*\.txt)',str(result.decode('utf-8')))):
+        # print(i)
+        hidFilesList.append(i)
+    # print(result)
+
+    return hidFilesList
