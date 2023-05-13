128c8f4c504e955345ae3db2af08d0fdf62f93e7
Samyukta1909 <sahoosamyukta@gmail.com>
2023-05-13 19:41:01 +0530

    Final Commit Hopefully!

--- domain.py
+++ domain.py
@@ -5,26 +5,32 @@ def WHOis(domain_name):
         if (bool(d.domain_name)):
             global domainList
             whois_info =whois.whois(domain_name)
+            dn = "Domain Name: "+ str(whois_info.domain_name)
             dr = "Registrar: "+ str(whois_info.registrar)
+            org = "Organization: "+ str(whois_info.org)
             ur = "URL: "+ str(whois_info.registrar_url)
             cd = "Creation Date: "+ str(whois_info.creation_date)
             ed = "Expiration Date: "+ str(whois_info.expiration_date)
             ud = "Updated Date: "+ str(whois_info.updated_date)
             em = "Registrant Email: "+ str(whois_info.emails)
-            # st = "Status: "+ str(whois_info.status)
+            ds = "dnssec: "+ str(whois_info.dnssec)
+            ad = "Address: "+ str(whois_info.address)
+            domainList.append(dn)
             domainList.append(dr)
+            domainList.append(org)
             domainList.append(ur)
             domainList.append(cd)
             domainList.append(ed)
             domainList.append(ud)
             domainList.append(em)
-            # domainList.append(st)
+            domainList.append(ds)
+            domainList.append(ad)
             
             return domainList
         
         else:
             domainList =[]
-            d="unregisterd url"
+            d="UNREGISTERED URL"
             domainList.append(d)
             return domainList 
 
